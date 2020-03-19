4979690ff7f481481e5ce153ffc0b4b89c0bf3db
Younes Baddich <younes.baddich@devoteam.com>
2020-03-20 00:23:44 +0100

    adding website

---
+++ precache-manifest.bfab4dd3a8a2db7504fd884c32fc6203.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74acae1b998cbb040fc9ff0563d4fbe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a3a5b21f181b27b09a",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1174be557d459cf90fcf",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f3a3a5b21f181b27b09a",
+    "url": "/static/js/2.d28e3889.chunk.js"
+  },
+  {
+    "revision": "8460ef3cc16d482165fbc3e05bb9084f",
+    "url": "/static/js/2.d28e3889.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1174be557d459cf90fcf",
+    "url": "/static/js/main.9deeb47d.chunk.js"
+  },
+  {
+    "revision": "9716b81b31392c57e6a6",
+    "url": "/static/js/runtime-main.c39b6d1e.js"
+  }
+]);
